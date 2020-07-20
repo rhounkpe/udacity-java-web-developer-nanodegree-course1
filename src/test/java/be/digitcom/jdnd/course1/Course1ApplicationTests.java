eec2c6ee45ae70202dc7e22b95d90226b91585eb
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-20 15:55:23 +0200

    Initial State

---
+++ src/test/java/be/digitcom/jdnd/course1/Course1ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digitcom.jdnd.course1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
